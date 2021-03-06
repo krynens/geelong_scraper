fa92af669532e1b63d60fbba9042f905976e679e
krynens <75901402+krynens@users.noreply.github.com>
2021-03-06 23:09:18 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,38 +1,38 @@
 import os
 os.environ["SCRAPERWIKI_DATABASE_NAME"] = "sqlite:///data.sqlite"
 
-from bs4 import BeautifulSoup
-from datetime import datetime
-import requests
 import scraperwiki
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
 
 today = datetime.today()
 
-for i in range(1, 50):
-    try:
-        print(f'Getting page {i}')
-        url = 'https://www.melbourne.vic.gov.au/building-and-development/property-information/planning-building-registers/Pages/town-planning-permits-register-search-results.aspx?AdvertisingOnly=on&page=' + str(i)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'lxml')
+url = 'https://www.geelongaustralia.com.au/advertisedplanning/default.aspx'
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'lxml')
 
-        table = soup.find('tbody')
-        rows = table.find_all('tr', class_='detail')
+table = soup.find('table', class_='table table-striped')
+table.find_all('tr')[0].decompose()
+rows = table.find_all('tr')
 
-        for row in rows:
-            record = {}
-            record['address'] = row.find('td', class_='column2').text
-            date_received_raw = row.find('td', class_='column3').text
-            record['date_received'] = datetime.strptime(
-                date_received_raw, "%d/%m/%Y").strftime("%Y-%m-%d")
-            record['date_scraped'] = today.strftime("%Y-%m-%d")
-            record['description'] = row.find('td', class_='column4').text
-            record['council_reference'] = row.find('td', class_='column1').text
-            record['info_url'] = 'https://www.melbourne.vic.gov.au' + \
-                str(row.find('td', class_='column1')).split('"')[5]
+for row in rows:
+    record = {}
+    suburb = row.find_all('td')[1].text
+    record['address'] = f'{row.find_all("td")[0].text}, {suburb}'.strip().title()
+    record['date_scraped'] = today.strftime("%Y-%m-%d")
+    info_url = 'https://www.geelongaustralia.com.au/advertisedplanning/' + \
+        str(row.find_all('td')[0]).split('"')[1]
+    rr = requests.get(info_url)
+    soupp = BeautifulSoup(rr.content, 'lxml')
+    record['description'] = soupp.find_all('p')[3].text.strip()
+    record['council_reference'] = row.find_all('td')[2].text
+    record['info_url'] = info_url
+    on_notice_from_raw = row.find_all('td')[3].text
+    record['on_notice_from'] = datetime.strptime(
+        on_notice_from_raw, '%d %b %Y').strftime("%Y-%m-%d")
+    on_notice_to_raw = row.find_all('td')[4].text
+    record['on_notice_to'] = datetime.strptime(on_notice_to_raw, '%d %b %Y').strftime("%Y-%m-%d")
 
-            scraperwiki.sqlite.save(
-                unique_keys=['council_reference'], data=record, table_name="data")
-
-    except:
-        print('Scraper finished.')
-        break
+    scraperwiki.sqlite.save(
+        unique_keys=['council_reference'], data=record, table_name="data")
