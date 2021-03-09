22b6ca4b291dbd286d4d028b5f1c08c2c9c585a1
krynens <thomkrynen@gmail.com>
2021-03-09 21:03:37 +1100

    a

--- scraper.rb
+++ scraper.rb
@@ -5,22 +5,42 @@ FileUtils.touch('data.sqlite')
 
 today = Time.now.strftime('%Y-%m-%d')
 
-url   = 'https://www.maribyrnong.vic.gov.au/Advertised-Planning-Applications'
+url   = 'https://www.geelongaustralia.com.au/advertisedplanning/default.aspx'
 agent = Mechanize.new
 page  = agent.get(url)
 
-table = page.search('article')
-rows = table.search('a')
+table1 = page.search('table.table.table-striped')[0]
+table1.search('tr')[0].remove
+rows1 = table1.search('tr')
+table2 = page.search('table.table.table-striped')[1]
+table2.search('tr')[0].remove
+rows2 = table2.search('tr')
 
-for row in rows do
+for row in rows1 do
   record = {}
-  record['address'] = row.search('p.list-item-address').text.strip
-  record['council_reference'] = row.search('p')[0].text.strip
+  suburb = row.search('td')[1].text.strip
+  record['address'] = row.search('td')[0].text.strip + ', ' + suburb
+  record['council_reference'] = row.search('td')[2].text.strip
   record['date_scraped'] = today
-  record['description'] = row.search('p')[3].text.strip.sub(/caf\u00E9/, 'cafe')
-  on_notice_to_raw = row.search('p')[2].text.strip.split('until ')[1].split(', ')[0]
-  record['on_notice_to'] = DateTime.strptime(on_notice_to_raw, '%d %B %Y').strftime('%Y-%m-%d')
-  record['info_url'] = row.to_s.split('"')[1]
+  record['on_notice_from'] = DateTime.strptime(row.search('td')[3].text.strip, '%d %b %Y').strftime('%Y-%m-%d')
+  record['on_notice_to'] = DateTime.strptime(row.search('td')[4].text.strip, '%d %b %Y').strftime('%Y-%m-%d')
+  link = 'https://www.geelongaustralia.com.au/advertisedplanning/' + row.search('a').to_s.split('"')[1]
+  record['info_url'] = link
+  page  = agent.get(link)
+  record['description'] = page.search('p')[3].text.strip
+  ScraperWiki.save_sqlite(['council_reference'], record)
+end
 
+for row in rows2 do
+  record = {}
+  suburb = row.search('td')[1].text.strip
+  record['address'] = row.search('td')[0].text.strip + ', ' + suburb
+  record['council_reference'] = row.search('td')[2].text.strip
+  record['date_scraped'] = today
+  record['on_notice_to'] = DateTime.strptime(row.search('td')[3].text.strip, '%d %b %Y').strftime('%Y-%m-%d')
+  link = 'https://www.geelongaustralia.com.au/advertisedplanning/' + row.search('a').to_s.split('"')[1]
+  record['info_url'] = link
+  page  = agent.get(link)
+  record['description'] = page.search('p')[3].text.strip
   ScraperWiki.save_sqlite(['council_reference'], record)
 end
